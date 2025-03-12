16051571ca5c566b2e8fbf697d67b69464b5f29e
Kacper Turek <kacper.turek@inqse.com>
2025-03-12 18:18:47 +0100

    Updated const api in api.js file and added .env file to src directory

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
-const api = axios.create( {
-    baseURL: import.meta.env.VITE_API_URL
-})
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  });
 
 api.interceptors.request.use(
     (config) => {
